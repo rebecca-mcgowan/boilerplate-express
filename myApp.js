850026b60ff61b8a63610f1fd8bc593dc4f87eef
Rebecca McGowan <rebmcgow@twitch.tv>
2022-06-30 07:46:47 -0600

    Send file

--- myApp.js
+++ myApp.js
@@ -1,10 +1,16 @@
 let express = require('express');
 let app = express();
-console.log("Hello World");
-app.get("/", function(req, res) {
-    res.send("Hello Express");
-});
+console.log('Hello World');
 
+//app.get('/', function(req, res) {
+//    res.send('Hello Express');
+//});
+
+absolutePath = __dirname + relativePath/File.ext;
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/views.index.html');
+});
 
 
 
