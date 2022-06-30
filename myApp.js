2e3f929cf9694f4bd19eac13b000499b5c0b1df8
Rebecca McGowan <rebmcgow@twitch.tv>
2022-06-30 08:03:47 -0600

    Add json API response

--- myApp.js
+++ myApp.js
@@ -10,6 +10,10 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/json', (req, res) => {
+    res.json({'message': 'Hello json'});
+});
+
 app.use('/public', express.static(__dirname + '/public'));
 
 
