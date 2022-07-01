9d4d6ac2d49ae58a6b93f7296af622a58e41c010
Rebecca McGowan <rebmcgow@twitch.tv>
2022-06-30 21:39:42 -0600

    t

--- myApp.js
+++ myApp.js
@@ -4,6 +4,7 @@ let express = require('express');
 let app = express();
 console.log('Hello World');
 
+var response = 'Hello json';
 if (process.env.MESSAGE_STYLE === 'uppercase') {
     console.log('success');
 }
@@ -16,13 +17,13 @@ app.get('/', function(req, res) {
 });
 
 app.get('/json', (req, res) => {
-    var response = "Hello json";
-    if (process.env.MESSAGE_STYLE === "uppercase") {
-        res.json({'message': response.toUpperCase()});
-    } else {
-        res.json({'message': process.env.MESSAGE_STYLE});
-    }
-    res.json({"message": process.env.MESSAGE_STYLE});
+    //var response = "Hello json";
+    //if (process.env.MESSAGE_STYLE === 'uppercase') {
+    //    res.json({'message': response.toUpperCase()});
+    //} else {
+    //    res.json({'message': process.env.MESSAGE_STYLE});
+    //}
+    res.json({response});
 });
 
 console.log('test');
